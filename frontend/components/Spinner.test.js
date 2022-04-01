2e59d323a1911299a67d3ea050aa7b534063b2d3
GooieJS <conradklek@gmail.com>
2022-04-01 12:28:48 -0500

    submit

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -1,5 +1,12 @@
 // Import the Spinner component into this file and test
 // that it renders what it should for the different props it can take.
-test('sanity', () => {
-  expect(true).toBe(false)
+
+import React from 'react'
+import { render } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import Spinner from './Spinner'
+
+test('renders spinner', () => {
+  const { getByText } = render(<Spinner on />)
+  expect(getByText('Please wait...')).toBeInTheDocument()
 })
